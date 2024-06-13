799f60529f6182210071a9469a99a6c39903f90f
AltTTT <kiito.izumi@gmail.com>
2024-06-13 12:48:35 +0900

    sava

--- scripts/MyBaseController/Script.cs
+++ scripts/MyBaseController/Script.cs
@@ -14,6 +14,8 @@ using VRage.Game.ModAPI.Ingame;
 using VRage.Game.ObjectBuilders.Definitions;
 using SpaceEngineers.Game.ModAPI.Ingame;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 /*
  * Must be unique per each script project.
@@ -33,10 +35,11 @@ namespace MyBaseController {
          */
         #region MyBaseController
         public class BeanProvider {
-
+            //Repository
             #region Repository
-            private interface IRepository {
-                public void Load(IMyGridTerminalSystem gridTerminalSystem);
+
+            public interface IRepository {
+                void Load(IMyGridTerminalSystem gridTerminalSystem);
             }
             //Repositories
 
@@ -56,20 +59,29 @@ namespace MyBaseController {
                 }
 
             }
-            private List<IRepository> _repositories = new List<IRepository>();
-
             //Initialize Repositories
+            private List<IRepository> _repositories = new List<IRepository>();
+            private BatteryRepository _batteryRepository;
+            private TextPanelRepository _textPanelRepository;
             public void initRepositories() {
-                _repositories.Add(new BatteryRepository());
-                _repositories.Add(new TextPanelRepository());
+                _batteryRepository = new BatteryRepository();
+                _repositories.Add(_batteryRepository);
+                _textPanelRepository = new TextPanelRepository();
+                _repositories.Add(_textPanelRepository);
+            }
+            //method
+            public void LoadAllRepository(IMyGridTerminalSystem gridTerminalSystem) {
+                foreach (var repository in _repositories) {
+                    repository.Load(gridTerminalSystem);
+                }
             }
             #endregion Repository
-            #region  Service
-
             //Service
+            #region  Service
             private interface IService {
-                public void Update();
+                void Update();
             }
+            //Services
             private class BatteryService : IService {
                 private readonly BatteryRepository _repository;
                 private float _currentStoredPower = 0f;
@@ -95,22 +107,115 @@ namespace MyBaseController {
                 }
 
             }
+            private class TextPanelService : IService {
+                private readonly TextPanelRepository _repository;
+                public TextPanelService(TextPanelRepository repository) {
+                    _repository = repository;
+                }
+                public void Update() {
+                    foreach (var textPanel in _repository.TextPanels) {
+                        var cData = textPanel.CustomData;
+                        string[] lines = cData.Replace("\r\n", "\n").Split(new[] { '\n', '\r' });
+                        StringBuilder sb = new StringBuilder();
+                        foreach (var line in lines) {
+                            if (map.ContainsKey(line))
+                                sb.Append(map[line]);
+                        }
+                        textPanel.WriteText(sb.ToString());
+                    }
+                }
+                Dictionary<string, string> map = new Dictionary<string, string>();
+                public void send(string key, string value) {
+                    map[key] = value;
+                }
+
+            }
+            //Initialize Services
+            private List<IService> _services = new List<IService>();
+
+            private BatteryService _batteryService;
+            private TextPanelService _textPanelService;
+
+            public void initServices() {
+                _batteryService = new BatteryService(_batteryRepository);
+                _services.Add(_batteryService);
+                _textPanelService = new TextPanelService(_textPanelRepository);
+                _services.Add(_textPanelService);
+            }
+            //method
+            public void UpdateAllService() {
+                foreach (var service in _services) {
+                    service.Update();
+                }
+            }
             #endregion Service
+            //Controller
             #region  Controller
             private interface IController {
-                public void Apply();
+                void Apply();
+            }
+            //Controllers
+            private class BatteryController : IController {
+                private BatteryService _batteryService;
+                private TextPanelService _textPanelService;
+                public BatteryController(BatteryService batteryService, TextPanelService textPanelService) {
+                    _batteryService = batteryService;
+                    _textPanelService = textPanelService;
+                }
+                public void Apply() {
+                    int ratio = (int)Math.Round(_batteryService.StoredPowerRatio * 100);
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("Battery : " + ratio + "%");
+                    sb.Append("[");
+                    int i = 0;
+                    for (; i < ratio / 10; i++) {
+                        sb.Append("#");
+                    }
+                    for (; i < 10; i++) {
+                        sb.Append(" ");
+                    }
+                    sb.AppendLine("]");
+                    _textPanelService.send("Battery", sb.ToString());
+                }
+            }
+            //initialize Controllers
+            List<IController> _controllers = new List<IController>();
+            public void initControllers() {
+                _controllers.Add(new BatteryController(_batteryService, _textPanelService));
+            }
+            //method
+            public void ApplyAllController() {
+                foreach (var controller in _controllers) {
+                    controller.Apply();
+                }
             }
             #endregion Controller
 
         }
 
-        public Program() { }
+        private BeanProvider _beanProvider;
+        public Program() {
+            _beanProvider = new BeanProvider();
+
+            _beanProvider.initRepositories();
+            _beanProvider.initServices();
+            _beanProvider.initControllers();
+
+            _beanProvider.LoadAllRepository(GridTerminalSystem);
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
 
 
         public void Save() { }
 
 
-        public void Main(string argument, UpdateType updateSource) { }
+        public void Main(string argument, UpdateType updateSource) {
+            if ((updateSource & UpdateType.Update100) != 0) {
+                _beanProvider.UpdateAllService();
+                _beanProvider.ApplyAllController();
+            }
+        }
 
         #endregion // MyBaseController
     }
